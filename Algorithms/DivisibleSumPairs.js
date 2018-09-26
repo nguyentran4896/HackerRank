63d9877ea2fd1fdddf905af2daf71dc86758d385
nguyentran4896 <nguyentran4896@gmail.com>
2018-09-26 22:35:17 +0700

    Divisible Sum Pairs

---
+++ Algorithms/DivisibleSumPairs.js
@@ -0,0 +1,10 @@
+// Divisible Sum Pairs
+function divisibleSumPairs(n, k, ar) {
+  let count = 0
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if ((ar[i] + ar[j]) % k === 0) count++
+    }
+  }
+  return count
+}
